e47cc561a4e092ad9a96cd45659ad898c0be472d
damianhouse <damianhouse@gmail.com>
2016-02-03 16:33:27 -0500

    Changed aces to 22, WORKING

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,15 +1,10 @@
-def math(first_card, second_card)
-  sum = (first_card.to_i + second_card.to_i)
+require 'byebug'
+
+def math_stuff(first_card, second_card)
+  sum = (first_card.to_i) + (second_card.to_i)
+  return sum.to_s
 end
 
-puts "Please enter your first card"
-first_card = gets.chomp
-puts "Please enter your second card"
-second_card = gets.chomp
-puts "Please enter the dealer's card"
-dealer_card = gets.chomp.to_i
-user_total = math(first_card, second_card)
-puts "#{user_total}"
 hard = {"5" => ["H","H","H","H","H","H","H","H","H","H"],
         "6" => ["H","H","H","H","H","H","H","H","H","H"],
         "7" => ["H","H","H","H","H","H","H","H","H","H"],
@@ -39,13 +34,49 @@ soft = {"13" => ["H","H","Dh","Dh","Dh","H","H","H","H","H"],
         "20" => ["S","S","S","S","S","S","S","S","S","S"],
         "21" => ["S","S","S","S","S","S","S","S","S","S"]}
 
-pair = {"2" => ["P","P","P","P","P","P","H","H","H","H"],
-        "3" => ["P","P","P","P","P","P","P","H","H","H"],
-        "4" => ["H","H","P","P","P","H","H","H","H","H"],
-        "5" => ["Dh","Dh","Dh","Dh","Dh","Dh","Dh","Dh","H","H"],
-        "6" => ["P","P","P","P","P","P","H","H","H","H"],
-        "7" => ["P","P","P","P","P","P","P","H","S","H"],
-        "8" => ["P","P","P","P","P","P","P","P","P","P"],
-        "9" => ["P","P","P","P","P","S","P","P","S","S"],
-        "10" => ["S","S","S","S","S","S","S","S","S","S"],
-        "A" => ["P","P","P","P","P","P","P","P","P","P"]}
+pair = {"4" => ["P","P","P","P","P","P","H","H","H","H"],
+        "6" => ["P","P","P","P","P","P","P","H","H","H"],
+        "8" => ["H","H","P","P","P","H","H","H","H","H"],
+        "10" => ["Dh","Dh","Dh","Dh","Dh","Dh","Dh","Dh","H","H"],
+        "12" => ["P","P","P","P","P","P","H","H","H","H"],
+        "14" => ["P","P","P","P","P","P","P","H","S","H"],
+        "16" => ["P","P","P","P","P","P","P","P","P","P"],
+        "18" => ["P","P","P","P","P","S","P","P","S","S"],
+        "20" => ["S","S","S","S","S","S","S","S","S","S"],
+        "22" => ["P","P","P","P","P","P","P","P","P","P"]}
+
+puts "Please enter your first card"
+first_card = gets.chomp.upcase
+puts "Please enter your second card"
+second_card = gets.chomp.upcase
+puts "Please enter the dealer's card"
+dealer_card = gets.chomp.upcase
+
+if (first_card && second_card) == "A"
+  user_total = "22"
+elsif first_card == "A"
+  first_card = "11"
+  user_total = math_stuff(first_card, second_card)
+elsif second_card == "A"
+  second_card = "11"
+  user_total = math_stuff(first_card, second_card)
+else
+  user_total = math_stuff(first_card, second_card)
+end
+
+if dealer_card == "A"
+  dealer_card = "11"
+else
+  dealer_card.to_i
+end
+# byebug
+if first_card == second_card
+  puts "#{pair[user_total][(dealer_card.to_i - 2)]}"
+elsif first_card = "11" || second_card = "11"
+  puts "#{soft[user_total][(dealer_card.to_i - 2)]}"
+else
+  puts "#{hard[user_total][(dealer_card.to_i - 2)]}"
+end
+
+
+puts "#{user_total}"
